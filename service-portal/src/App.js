49e9721881218263a5003eabae32bb3f776e4877
Nikhil <onikhilkumar02@gmail.com>
2021-02-08 21:06:23 +0530

    started with the front-end

---
+++ service-portal/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>LETS START THE FRONT END HERE</h1>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
