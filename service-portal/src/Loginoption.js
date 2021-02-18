92610acf9ca781f4e1577a85bab0635e68604968
@Hell3ringer <vamshiduvva23@gmail.com>
2021-02-18 21:27:08 +0530

    added login

---
+++ service-portal/src/Loginoption.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route} from 'react-router-dom'
+import './Loginoption.css';
+
+
+
+
+const Loginpage = () => {    
+
+    return (
+        <div>
+            < h1 style={{color:'gray'}}> Login page</h1 >            
+            
+            <div className='button'>
+                <Route render={({ history }) => (
+                    <button className='btn1' onClick={() => { history.push('/login') }}>
+                        seeker
+                    </button>
+                )} />
+                <Route render={({ history }) => (
+                    <button className='btn2' onClick={() => { history.push('/login') }}>
+                        giver
+                    </button>
+                )} />
+            </div>
+            
+        </div>
+
+)
+
+}
+
+export default Loginpage
