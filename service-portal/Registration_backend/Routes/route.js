487b87662d3db053e3e08c9f79263001b8671e65
@Hell3ringer <vamshiduvva23@gmail.com>
2021-02-25 00:31:50 +0530

    assigining button remaining

--- service-portal/Registration_backend/Routes/route.js
+++ service-portal/Registration_backend/Routes/route.js
@@ -1,10 +1,57 @@
-const { Router, request, response } = require('express')
+
 const express = require('express')
 const router = express.Router()
 const signupTemplatecopy = require('../models/Signup_model')
+
+
 const bcrypt = require('bcrypt')
+const { Redirect, Route } = require('react-router')
 
 
+router.post('/login',async (req,res) => {
+    //const { loginname , loginpassword } = req.body
+    loginname = req.body.loginName
+    loginpassword=req.body.loginPassword
+    
+    // signupTemplatecopy.find({
+    //     'fullName':name
+    // },{
+    //     'email':1,'_id':0
+    // })
+    console.log(loginpassword + 'kkkkkkkkk');
+    const user = await signupTemplatecopy.findOne({
+        'fullName':loginname
+    }).lean()
+
+    if(!user){
+        console.log('no user');
+        return false; 
+    }
+    if(await bcrypt.compare(loginpassword,user.password)){
+        console.log('yesssss');
+        flagroute = true;  
+        
+    }else{
+        console.log('no');
+         
+    }
+    
+})
+// router.get('/login',(req,res) => {
+//     var email = 'a@gmail.com'
+//     signupTemplatecopy.find({
+//         'fullName':email
+//     },{
+//         'experience':1,'_id':0
+//     })
+//     .then(data => {
+//         res.json(data)
+//     })
+//     .catch(error => {
+//         res.json(error)
+//     })
+
+// })
 
 router.post('/signup',async (request,response) => {
 
@@ -28,5 +75,7 @@ router.post('/signup',async (request,response) => {
         response.json(error)
     })
 })
+
+
 
 module.exports = router
